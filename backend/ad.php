94781f9d2dfb37ae0e0d98b99013196f6232dabe
Sylvester Chen <sylvesterexba@gmail.com>
2021-06-30 00:42:45 +0800

    完成後台各頁面及彈出視窗html碼撰寫

--- backend/ad.php
+++ backend/ad.php
@@ -1,6 +1,6 @@
 <div style="width:99%; height:87%; margin:auto; overflow:auto; border:#666 1px solid;">
   <p class="t cent botli"><?=$ts[$do];?></p>
-  <form method="post" target="back" action="?do=tii">
+  <form method="post" action="api/edit.php">
     <table width="100%">
       <tbody>
         <tr class="yel">
@@ -15,8 +15,10 @@
     <table style="margin-top:40px; width:70%;">
       <tbody>
         <tr>
-          <td width="200px"><input type="button"
-              onclick="op(&#39;#cover&#39;,&#39;#cvr&#39;,&#39;view.php?do=title&#39;)" value="新增網站標題圖片"></td>
+          <td width="200px">
+            <input type="button" onclick="op(&#39;#cover&#39;,&#39;#cvr&#39;,&#39;modal/<?=$do;?>.php&#39;)"
+              value="<?=$as[$do];?>">
+          </td>
           <td class="cent"><input type="submit" value="修改確定"><input type="reset" value="重置"></td>
         </tr>
       </tbody>
@@ -25,21 +27,3 @@
   </form>
 </div>
 </div>
-<div id="alt"
-  style="position: absolute; width: 350px; min-height: 100px; word-break:break-all; text-align:justify;  background-color: rgb(255, 255, 204); top: 50px; left: 400px; z-index: 99; display: none; padding: 5px; border: 3px double rgb(255, 153, 0); background-position: initial initial; background-repeat: initial initial;">
-</div>
-<script>
-$(".sswww").hover(
-  function() {
-    $("#alt").html("" + $(this).children(".all").html() + "").css({
-      "top": $(this).offset().top - 50
-    })
-    $("#alt").show()
-  }
-)
-$(".sswww").mouseout(
-  function() {
-    $("#alt").hide()
-  }
-)
-</script>
