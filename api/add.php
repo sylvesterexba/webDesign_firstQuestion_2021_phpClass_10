45c21820b2882361da6aecc2035ee0f6cde7e5f2
Sylvester Chen <sylvesterexba@gmail.com>
2021-06-30 20:17:43 +0800

    後台標題圖片除厝完成

--- api/add.php
+++ api/add.php
@@ -6,7 +6,7 @@ $db=new DB($_POST['table']);
 if (isset($_FILES['img']['tmp_name'])) {
   // echo $_FILES['img']['tmp_name'];
     move_uploaded_file($_FILES['img']['tmp_name'], "../img/".$_FILES['img']['name']);
-    $data['img']=$_FILES['img']['tmp_name'];
+    $data['img']=$_FILES['img']['name'];
 
 }
 
