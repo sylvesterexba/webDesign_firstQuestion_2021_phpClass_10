7b1d0a786866be34711a24aa6638c63f477fb73c
Sylvester Chen <sylvesterexba@gmail.com>
2021-07-04 02:22:48 +0800

    完成前台動畫輪播圖顯示

--- front/main.php
+++ front/main.php
@@ -2,30 +2,46 @@
   style="height:540px; border:#999 1px solid; width:53.2%; margin:2px 0px 0px 0px; float:left; position:relative; left:20px;">
   <!-- 跑馬燈物件 -->
   <?php include "./front/marquee.php";?>
-
   <div style="height:32px; display:block;"></div>
   <!--正中央-->
+
+  <div style="width:100%; padding:2px; height:290px;">
+        <div id="mwww" loop="true" style="width:100%; height:100%;">
+            <div style="width:99%; height:100%; position:relative;" class="cent">沒有資料</div>
+        </div>
+    </div>
+    
   <script>
   var lin = new Array();
+
+  <?php
+    $mvs=$Mvim->all(['sh'=>1]);
+    foreach ($mvs as $mv) {
+        echo "lin.push('img/{$mv['img']}');" ;
+    }
+        //lin.push();lin.push();lin.push()
+    ?>
+
+    //console.log(lin);
   var now = 0;
+  ww();// 完善輪播功能 
+
   if (lin.length > 1) {
     setInterval("ww()", 3000);
     now = 1;
   }
 
   function ww() {
-    $("#mwww").html("<embed loop=true src='" + lin[now] + "' style='width:99%; height:100%;'></embed>")
-    //$("#mwww").attr("src",lin[now])
-    now++;
-    if (now >= lin.length)
-      now = 0;
-  }
+        $("#mwww").html("<embed loop=true src='" + lin[now] + "' style='width:99%; height:100%;'></embed>")
+        //$("#mwww").attr("src",lin[now])
+        /* console.log("放圖"+now) */
+        now++;
+        if (now >= lin.length)
+            now = 0;
+    }
+
   </script>
-  <div style="width:100%; padding:2px; height:290px;">
-    <div id="mwww" loop="true" style="width:100%; height:100%;">
-      <div style="width:99%; height:100%; position:relative;" class="cent">沒有資料</div>
-    </div>
-  </div>
+
   <div
     style="width:95%; padding:2px; height:190px; margin-top:10px; padding:5px 10px 5px 10px; border:#0C3 dashed 3px; position:relative;">
     <span class="t botli">最新消息區
