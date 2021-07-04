877504a83cfbebcbd757d84785b2a04f4a77bfe6
Sylvester Chen <sylvesterexba@gmail.com>
2021-07-04 12:08:29 +0800

    首頁最新消息顯示

--- front/main.php
+++ front/main.php
@@ -6,11 +6,11 @@
   <!--正中央-->
 
   <div style="width:100%; padding:2px; height:290px;">
-        <div id="mwww" loop="true" style="width:100%; height:100%;">
-            <div style="width:99%; height:100%; position:relative;" class="cent">沒有資料</div>
-        </div>
+    <div id="mwww" loop="true" style="width:100%; height:100%;">
+      <div style="width:99%; height:100%; position:relative;" class="cent">沒有資料</div>
     </div>
-    
+  </div>
+
   <script>
   var lin = new Array();
 
@@ -22,9 +22,9 @@
         //lin.push();lin.push();lin.push()
     ?>
 
-    //console.log(lin);
+  //console.log(lin);
   var now = 0;
-  ww();// 完善輪播功能 
+  ww(); // 完善輪播功能 
 
   if (lin.length > 1) {
     setInterval("ww()", 3000);
@@ -32,21 +32,36 @@
   }
 
   function ww() {
-        $("#mwww").html("<embed loop=true src='" + lin[now] + "' style='width:99%; height:100%;'></embed>")
-        //$("#mwww").attr("src",lin[now])
-        /* console.log("放圖"+now) */
-        now++;
-        if (now >= lin.length)
-            now = 0;
-    }
-
+    $("#mwww").html("<embed loop=true src='" + lin[now] + "' style='width:99%; height:100%;'></embed>")
+    //$("#mwww").attr("src",lin[now])
+    /* console.log("放圖"+now) */
+    now++;
+    if (now >= lin.length)
+      now = 0;
+  }
   </script>
 
   <div
     style="width:95%; padding:2px; height:190px; margin-top:10px; padding:5px 10px 5px 10px; border:#0C3 dashed 3px; position:relative;">
     <span class="t botli">最新消息區
+    <?php
+        if($News->count(['sh'=>1])>5){
+          echo "<a href='index.php?do=news' style='float:right'>More...</a>";
+        }
+
+    ?>
     </span>
     <ul class="ssaa" style="list-style-type:decimal;">
+      <?php
+        $ns=$News->all(['sh'=>1]," Limit 5 ");
+        foreach ($ns as $news) {
+          echo "<li>";
+          echo mb_substr($news['text'],0,20);
+          echo "<span class='all' style='display:none'>{$news['text']}</span>";
+          echo "</li>";
+        }
+        
+      ?>
     </ul>
     <div id="altt"
       style="position: absolute; width: 350px; min-height: 100px; background-color: rgb(255, 255, 204); top: 50px; left: 130px; z-index: 99; display: none; padding: 5px; border: 3px double rgb(255, 153, 0); background-position: initial initial; background-repeat: initial initial;">
